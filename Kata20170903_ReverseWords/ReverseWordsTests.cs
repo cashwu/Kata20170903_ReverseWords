f9b26b958c12e89c864761e41bfd1f8a1a3202ea
cash <cash@cashwu.com>
2017-09-03 21:12:38 +0800

    input a test case

--- Kata20170903_ReverseWords/ReverseWordsTests.cs
+++ Kata20170903_ReverseWords/ReverseWordsTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170903_ReverseWords
     public class ReverseWordsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_a_shuold_return_a()
         {
+            ReverseWordsShouldBe("a", "a");
+        }
+
+        private static void ReverseWordsShouldBe(string expected, string str)
+        {
+            var kata = new Kata();
+            var actual = kata.ReverseWords(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string ReverseWords(string str)
+        {
+            return str;
         }
     }
 }
