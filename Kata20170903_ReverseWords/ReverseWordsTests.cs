faff187761e63dc0f646109375c7e40546ebc74e
cash <cash@cashwu.com>
2017-09-03 21:14:16 +0800

    input ab test case

--- Kata20170903_ReverseWords/ReverseWordsTests.cs
+++ Kata20170903_ReverseWords/ReverseWordsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170903_ReverseWords
@@ -10,6 +11,12 @@ namespace Kata20170903_ReverseWords
         public void input_a_shuold_return_a()
         {
             ReverseWordsShouldBe("a", "a");
+        }
+
+        [TestMethod]
+        public void input_ab_shuold_return_ba()
+        {
+            ReverseWordsShouldBe("ab", "ba");
         }
 
         private static void ReverseWordsShouldBe(string expected, string str)
@@ -24,7 +31,7 @@ namespace Kata20170903_ReverseWords
     {
         public string ReverseWords(string str)
         {
-            return str;
+            return string.Concat(str.Reverse());
         }
     }
 }
